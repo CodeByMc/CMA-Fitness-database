5a070ba9ece08d7cc027fc3fc96fdf075cd3510b
CCV254 <celumusa99@gmail.com>
2026-04-26 21:20:24 +0200

    Update CMA FITNESS.sql

--- CMA FITNESS.sql
+++ CMA FITNESS.sql
@@ -6,11 +6,3 @@ CREATE TABLE Membership_Package (
     monthly_fee       NUMBER(8,2) NOT NULL
 =======
 
-CREATE TABLE Trainer (
-    trainer_id      NUMBER(5) PRIMARY KEY,
-    first_name      VARCHAR2(50) NOT NULL,
-    last_name       VARCHAR2(50) NOT NULL,
-    specialization  VARCHAR2(100),
-    session_rate    NUMBER(8,2)
->>>>>>> 46648df462ef4cde1a82d54e00d18f6c4a53cef8
-);
